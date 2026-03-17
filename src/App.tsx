a6002f81dd5f6d09f42715bf901ee4734e6fe008
Cyril Khan <cyrilkhan@users.noreply.github.com>
2026-03-17 09:00:31 +0100

    Stufe 5: useEffect + localStorage mit sauberer Helper-Funktionen Struktur

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import './index.css';
 import InventoryList from "./components/InventoryList.tsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Item } from "./utilities/interfaces.ts";
 import AddItemForm from "./components/AddItemForm.tsx";
-import { generateId } from "./utilities/helper-functions.ts";
+import { generateId, getStoredItems, storageKeys } from "./utilities/helper-functions.ts";
 
 function App() {
-  const [items, setItems] = useState<Item[]>([
-    { id: 1, name: 'Laptop', quantity: 3 },
-    { id: 3, name: 'Monitor', quantity: 5 },
-    { id: 5, name: 'Chair', quantity: 7 }
-  ])
+  const [items, setItems] = useState<Item[]>(getStoredItems())
+
+  useEffect(() => {
+    localStorage.setItem(storageKeys.INVENTORY, JSON.stringify(items));
+  }, [items])
 
   const increaseQuantity = (id: number) => {
     setItems(prev => prev.map(item => item.id === id ? { ...item, quantity: item.quantity + 1 } : item))
--- src/utilities/helper-functions.ts
+++ src/utilities/helper-functions.ts
@@ -3,3 +3,25 @@ import type { Item } from "./interfaces.ts";
 export const generateId = (items: Item[]): number => {
   return items.length === 0 ? 1 : Math.max(...items.map(item => item.id), 0) + 1;
 }
+
+export const storageKeys = {
+  INVENTORY: 'app-inventory-items'
+} as const;
+
+export const getDefaultItems = (): Item[] => {
+  return [
+    { id: 1, name: 'Laptop', quantity: 3 },
+    { id: 3, name: 'Monitor', quantity: 5 },
+    { id: 5, name: 'Chair', quantity: 7 }
+  ]
+}
+
+export const getStoredItems = (): Item[] => {
+  const storageItems = localStorage.getItem(storageKeys.INVENTORY);
+
+  if (storageItems) {
+    return JSON.parse(storageItems);
+  }
+
+  return getDefaultItems();
+}
